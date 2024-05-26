0691d6c6a4094ba1024572e9016880e7fcdb8914
bigtr3 <tmddms1002@gmail.com>
2024-05-26 22:37:36 +0900

    [Feat]: 기본 설정 완료.

---
+++ src/main/java/com/example/petshield/PetshieldApplication.java
@@ -0,0 +1,13 @@
+package com.example.petshield;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetshieldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetshieldApplication.class, args);
+	}
+
+}
